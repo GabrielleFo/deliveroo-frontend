7fed022db31667e707e3e5ca851258d651e6e2a7
FOUIX <fouix@pc-155.home>
2020-11-10 13:46:00 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [food, setFood] = useState({});
+  const [isLoading, setIsloading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get("https://deliveroob.herokuapp.com/");
+    console.log(response.data);
+    setFood(response.data);
+    setIsloading(false);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      {isLoading === true ? (
+        <p>En cours de chargement ...</p>
+      ) : (
+        <div className="montorgueil">
+          <h2>{food.restaurant.name}</h2>
+          <p>{food.restaurant.description}</p>
+          <img src={food.restaurant.picture} alt={food.restaurant.title} />
+          <h3>{food.categories.name}</h3>
+        </div>
+      )}
     </div>
   );
 }
