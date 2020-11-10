3a6fc2dd91e8c9fdbf952838dcdfecfd0beca38a
FOUIX <fouix@pc-155.home>
2020-11-10 15:13:44 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import axios from "axios";
 
 function App() {
   const [food, setFood] = useState({});
+
   const [isLoading, setIsloading] = useState(true);
 
   const fetchData = async () => {
     const response = await axios.get("https://deliveroob.herokuapp.com/");
     console.log(response.data);
     setFood(response.data);
+
     setIsloading(false);
   };
   useEffect(() => {
@@ -21,12 +23,21 @@ function App() {
       {isLoading === true ? (
         <p>En cours de chargement ...</p>
       ) : (
-        <div className="montorgueil">
-          <h2>{food.restaurant.name}</h2>
-          <p>{food.restaurant.description}</p>
-          <img src={food.restaurant.picture} alt={food.restaurant.title} />
-          <h3>{food.categories.name}</h3>
-        </div>
+        <>
+          <div className="montorgueil">
+            <h2>{food.restaurant.name}</h2>
+            <p>{food.restaurant.description}</p>
+            <img src={food.restaurant.picture} alt={food.restaurant.title} />
+          </div>
+          <div className="categories">
+            <h3>{food.categories[0].name}</h3>
+            <h4>
+              {food.categories.map((elem, index) => {
+                return <div> {elem.name}</div>;
+              })}
+            </h4>
+          </div>
+        </>
       )}
     </div>
   );
